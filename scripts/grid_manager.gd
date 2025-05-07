69b54261e8f08c9fd9ac3762e4d1a99d87e7d9b8
dexmcc <32705908+dexmcc@users.noreply.github.com>
2025-05-07 17:09:30 +0100

    worked on player down and moving, some bugs tho

--- scripts/grid_manager.gd
+++ scripts/grid_manager.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal board_check_delay
+
 #loading the grid nodes to instantiate them
 @onready var grid_node_scene = load("res://Scenes/grid_node.tscn")
 @onready var puyo_scene = load("res://Scenes/puyo.tscn")
@@ -15,25 +17,53 @@ extends Node2D
 #just remember that we go through 0->rows then each time 0->columns + 2 (this is for stupid box)
 var grid : Array = Array()
 var puyos_to_pop : Array = Array()
+
 #the first puyo in the player array is the 'pivot'
 var player_puyos: Array = Array()
 var player_grid_positions: Array = Array()
-#player rotation is -> 0 V 90 <-180 ^ 270
+#player rotation is right:0 down:1 left:2 up:3 
 var player_rotation : int = 0
+var puyo_queue : Array[Array] = []
+#helps with delaying creating a player
+var player_create_flag = false
 
 @export var down_tick_speed : float = 0.2
+@export var player_down_speed : float = 0.6
 var chain_length : int = 0
 
+#this is a translation vector, not the position
+var next_input_move : Vector2i = Vector2i.ZERO
+var next_grid_positions : Array[Vector2i]
+
 func _ready():
+	$PlayerDownTimer.set_wait_time(player_down_speed)
 	initialize_grid()
-	fill_grid(11)
-	create_player_puyo()
+	fill_grid(12)
+	fill_puyo_queue()
 	await get_tree().create_timer(1).timeout
 	#calling this initiates a board check and allows chaining
-	await check_board(get_grouped_puyos())
+	check_board(get_grouped_puyos())
+	await board_check_delay
 	
-	
-	
+	#create_player_puyo()
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("puyo_left"):
+		next_input_move = Vector2i.LEFT
+		next_grid_positions = [(player_grid_positions[0] + next_input_move), (player_grid_positions[1] + next_input_move)]
+		if check_next_move(next_grid_positions):
+			player_move(next_grid_positions)
+	elif Input.is_action_just_pressed("puyo_right"):
+		next_input_move = Vector2i.RIGHT
+		next_grid_positions = [(player_grid_positions[0] + next_input_move), (player_grid_positions[1] + next_input_move)]
+		if check_next_move(next_grid_positions):
+			player_move(next_grid_positions)
+	elif Input.is_action_just_pressed("puyo_rotate"):
+		player_rotate()
+	elif Input.is_action_just_pressed("puyo_down"):
+		print("down press")
+	elif Input.is_action_just_released("puyo_down"):
+		print("down release")
 
 func fill_grid(how_full : int):
 	for i in range(0, grid_width):
@@ -44,8 +74,6 @@ func fill_grid(how_full : int):
 			node_to_fill.set_puyo(puyo)
 			node_to_fill.set_type(randi_range(2,5))
 			
-		
-	
 
 #initialises the grid and gives all of the nodes their neighbours
 func initialize_grid():
@@ -131,18 +159,19 @@ func get_grouped_puyos() -> Array:
 				node_group[n].is_checked = true
 			
 	print(str("found ", groups.size(), " groups"))
-	
 	#set all nodes in the group as unchecked
 	for i in range(0, grid_width):
 		for j in range(0, grid_height):
 			grid[i][j].is_checked = false
-	
+	down_tick()
+	board_check_delay.emit()
 	return groups
 
 #called when a tick for gravity needs to be checked, callls itself if it changed anything
 func down_tick() -> bool:
 	#sets a check to see if anything changed, if it hasn't changed it should stay false
 	var check = false
+	await get_tree().create_timer(down_tick_speed).timeout
 	#loops through all puyos, but from bottom up
 	for i in range(0, grid_width):
 		#starts with one above bottom
@@ -193,10 +222,11 @@ func pop_puyos(puyo_groups:= puyos_to_pop):
 	puyos_to_pop = Array()
 
 #starts a board check loop
-func check_board(puyos_to_pop : Array):
+func check_board(puyos_to_pop : Array) -> bool:
 	if puyos_to_pop.is_empty():
 		print("chain: ", chain_length)
 		chain_length = 0
+		return false
 	else:
 		chain_length += 1
 		pop_puyos(puyos_to_pop)
@@ -205,23 +235,79 @@ func check_board(puyos_to_pop : Array):
 			down_check = await down_tick()
 		await get_tree().create_timer(1).timeout
 		check_board(get_grouped_puyos())
+		return true
 
 #creates player puyo
 func create_player_puyo():
-	player_puyos = [puyo_scene.instantiate(), puyo_scene.instantiate()]
+	player_create_flag = false
+	
+	player_puyos = puyo_queue.pop_front()
+	fill_puyo_queue()
 	add_child(player_puyos[0])
 	add_child(player_puyos[1])
+	
 	player_rotation = 0
-	player_puyos[0].set_type(randi_range(2,5))
-	player_puyos[1].set_type(randi_range(2,5))
+	
 	player_grid_positions = [Vector2i(int ((grid_width -1)/ 2),0), Vector2i(int ((grid_width -1)/ 2) + 1,0)]
+	
 	player_puyos[0].global_position.x = grid[int (grid_width / 2)][0].position.x + square_size
 	player_puyos[1].global_position.x  = grid[(int (grid_width / 2)) + 1][0].position.x + square_size * 2
+	
+	$PlayerDownTimer.set_wait_time(player_down_speed)
+	$PlayerDownTimer.start()
 
 #returns true if next move is valid
-func check_next_move(next_move_positions: Array[Vector2i]) -> bool:
-	for pos in next_move_positions:
-		print(grid[pos.x][pos.y].is_holding_puyo)
+func check_next_move(next_grid_positions: Array[Vector2i]) -> bool:
+	for pos in next_grid_positions:
+		if (pos.x > grid_width - 1) or (pos.x < 0) or (pos.y > grid_height - 1) or (pos.y < 0):
+			return false
 		if grid[pos.x][pos.y].is_holding_puyo:
 			return false
 	return true
+
+#assumes move being given to it is valid
+func player_move(new_grid_positions: Array[Vector2i]):
+	var translation_difference = new_grid_positions[0] - player_grid_positions[0]
+	translation_difference *= square_size
+	for moving_puyo in player_puyos:
+		moving_puyo.global_position.x += translation_difference.x * 2
+		moving_puyo.global_position.y += translation_difference.y * 2
+	player_grid_positions = new_grid_positions
+
+#connected to the timer via signal
+func player_down_tick():
+	var next_moves : Array[Vector2i] = []
+	for player_position in player_grid_positions:
+		next_moves.append(Vector2i(player_position.x, player_position.y + 1))
+	if check_next_move(next_moves):
+		player_move(next_moves)
+	else:# if player shouldn't be able to move, turn it into part of the grid
+		$PlayerDownTimer.stop()
+		for i in range(0,2):
+			var new_puyo_base = player_puyos[i]
+			var new_puyo_position = Vector2(player_grid_positions[i])
+			var node_to_fill = grid[new_puyo_position.x][new_puyo_position.y]
+			var new_puyo : Puyo = puyo_scene.instantiate()
+			new_puyo.set_type(new_puyo_base.get_type())
+			node_to_fill.add_child(new_puyo)
+			node_to_fill.set_puyo(new_puyo)
+			player_puyos[i].queue_free()
+		player_puyos.clear()
+		await check_board(get_grouped_puyos())
+		create_player_puyo()
+		#once the board is updated, make a new player
+
+func player_rotate():
+	var rotation_check : int = (player_rotation + 1) % 4
+	var new_rotation_check : Vector2i
+	#the puyo pair rotates around a pivot so we only need to check the validity of rotating
+	##DO THIS WITH MAR BC I DONT WANNA WASTE A BUNCH OF TIME TRYING TO REMEMBER VECTOR MATHS
+	pass
+
+func fill_puyo_queue():
+	while puyo_queue.size() <= 2:
+		var new_puyos = [puyo_scene.instantiate(), puyo_scene.instantiate()]
+		new_puyos[0].set_type(randi_range(2,5))
+		new_puyos[1].set_type(randi_range(2,5))
+		puyo_queue.append(new_puyos)
+		print("2 pairs away:",new_puyos[0].get_type(), " ", new_puyos[1].get_type())
