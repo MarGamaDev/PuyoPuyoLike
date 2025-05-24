51bef653f54ad81bc2059ebc91212dafb00e78ca
dexmcc <32705908+dexmcc@users.noreply.github.com>
2025-05-24 14:16:50 +0100

    added foresight spell

--- scripts/spells/brutality_spell.gd
+++ scripts/spells/brutality_spell.gd
@@ -8,6 +8,7 @@ signal deal_spell_damage(damage : int)
 func connect_to_effect_signals():
 	deal_spell_damage.connect(combat_manager.damage_targeted_enemy)
 
+
 func trigger_spell_effect():
 	deal_spell_damage.emit(spell_damage)
 	print("Brutality cast")
---
+++ scripts/spells/foresight_spell.gd
@@ -0,0 +1,16 @@
+extends SequentialSpell
+
+##deals current counter as damage to targeted enemy at end of the chain
+signal deal_spell_damage(damage : int)
+
+func connect_to_effect_signals():
+	deal_spell_damage.connect(combat_manager.damage_targeted_enemy)
+
+func trigger_spell_effect():
+	puyo_manager.on_chain_ending.connect(counter_deal_damage)
+
+func counter_deal_damage(chain_mult_unused: int):
+	puyo_manager.on_chain_ending.disconnect(counter_deal_damage)
+	var counter_damage = player.counter
+	deal_spell_damage.emit(counter_damage)
+	print("foresight cast for %s" % counter_damage)
