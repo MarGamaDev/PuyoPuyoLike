ccc406260d199d2c5c7f1df2b44dd14d804f3fc8
dexmcc <32705908+dexmcc@users.noreply.github.com>
2025-05-25 19:14:52 +0100

    fix for relics

--- Scripts/Relics/System/relic_manager.gd
+++ Scripts/Relics/System/relic_manager.gd
@@ -1,14 +1,20 @@
 class_name RelicManager extends Node
 
+func _ready():
+	add_relic(load("res://Resources/RelicData/afterburner.tres"))
+
 func add_relic(relic_data: RelicData) -> void:
-	var new_relic = load(relic_data.filepath)
-	new_relic.reparent(self)
+	var new_relic = await load(relic_data.file_path).instantiate()
+	add_child(new_relic)
+	#new_relic.reparent(self)
 	new_relic.initialize()
 	add_relic_visual(relic_data.sprite)
 	pass
 
 func add_relic_visual(sprite: Texture) -> void:
 	var new_visual = TextureRect.new()
+	new_visual.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	new_visual.expand_mode = TextureRect.EXPAND_KEEP_SIZE
 	new_visual.texture = sprite
-	$RelicContainer.add_child(new_visual)
+	$CanvasLayer/RelicContainer.add_child(new_visual)
 	pass
--- Scripts/Reward.gd
+++ Scripts/Reward.gd
@@ -2,9 +2,10 @@ extends Resource
 
 class_name Reward
 
-enum REWARD_TYPE{SPELL, ITEM}
+enum REWARD_TYPE{SPELL, RELIC}
 @export var reward_type : REWARD_TYPE
 @export var spell_data : SpellData
+@export var relic_data : RelicData
 var has_been_taken : bool = false
 
 static func create_spell_reward(spell : SpellData) -> Reward:
@@ -13,4 +14,10 @@ static func create_spell_reward(spell : SpellData) -> Reward:
 	instance.spell_data = spell
 	instance.has_been_taken = false
 	return instance
-	
+
+static func create_relic_reward(relic :RelicData) -> Reward:
+	var instance = Reward.new()
+	instance.reward_type = REWARD_TYPE.RELIC
+	instance.relic_data = relic
+	instance.has_been_taken = false
+	return instance
