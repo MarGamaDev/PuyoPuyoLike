77aa23b885a8efc5ed15651d19ff8f7e914875ca
MarGama <94371820+MarGamaDev@users.noreply.github.com>
2025-05-25 20:14:10 +0200

    Revert "type"
    This reverts commit ffafe0aabeafcb450a502151fbda94a0826d8e56.

--- Scripts/Relics/Variants/afterburner_relic.gd
+++ Scripts/Relics/Variants/afterburner_relic.gd
@@ -2,7 +2,7 @@ class_name AfterBurner extends BaseRelic
 
 func initialize() -> void:
 	super()
-	get_node("root/Combat/PuyoManager").connect("player_attack", enhance_red_damage)
+	get_node("root/Combat/PuyoManager").connect("player_attacl", enhance_red_damage)
 
 func enhance_red_damage(attack: PlayerAttack) -> void:
 	if attack.type == Puyo.PUYO_TYPE.RED:
