3440dc848350db016c162b978b2d5e8bf15ae64e
dexmcc <32705908+dexmcc@users.noreply.github.com>
2025-05-29 21:57:33 +0100

    started workign on some spell fx stuff

---
+++ Scenes/effects/spell_effect.gd
@@ -0,0 +1,16 @@
+extends GPUParticles2D
+
+var sprite_path_prefix : String = "res://Art/spell elements/SpellRunes/puyo-spell"
+var sprite_path_suffix : String = "=.png"
+var sprite_path : String
+
+func _ready():
+	sprite_path = sprite_path_prefix + str(randi_range(1,12)) + sprite_path_suffix
+	texture = load(sprite_path)
+	restart()
+
+func start():
+	pass
+
+func _on_finished() -> void:
+	queue_free()
